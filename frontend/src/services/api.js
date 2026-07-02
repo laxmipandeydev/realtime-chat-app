858bad0c7a8c312c22892420b9623a07b32f18d7
laxmipandeydev <laxmipandey2816@gmail.com>
2026-07-02 21:35:02 +0530

    Fix API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://realtime-chat-app-y7q2.onrender.com",
+  baseURL: "https://realtime-chat-app-y7q2.onrender.com/api",
 });
 
 export default API;
