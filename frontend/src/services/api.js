b424ee915610a4f5cd6e33d92d8ec340037cc382
laxmipandeydev <laxmipandey2816@gmail.com>
2026-07-02 21:26:14 +0530

    Upadate API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://realtime-chat-app-y7q2.onrender.com",
 });
 
 export default API;
